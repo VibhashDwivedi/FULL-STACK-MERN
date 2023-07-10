dd94c7c19d43852919ba8e7b70216d1116295428
VibhashDwivedi <vibhashdwi1005@gmail.com>
2023-07-10 14:01:33 +0530

    Basics

--- Section1/index.js
+++ Section1/index.js
@@ -1,1 +1,20 @@
 console.log('Hello Javascript!');
+
+num=347;
+console.log(num);
+myname="Vibhash Dwivedi";
+console.log(myname);
+isLoggedin= false;
+console.log(typeof isLoggedin);
+console.log(typeof num);
+
+if(num%2 === 0){
+    console.log('Even');
+}
+else{
+    console.log('Odd');
+    a= 364567;
+}
+
+console.log(a);
+
---
+++ Section1/keywords.js
@@ -0,0 +1,14 @@
+age =30;
+
+if(age>=18){
+    console.log('Eligible for DL');
+    let x='this should be local';
+
+    const pi=3.14;
+    pi=3.15;
+}
+else{
+    console.log('Not eligible for DL');
+}
+
+console.log(x);
