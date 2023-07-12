7239b7d9c5209d1e707063100934cdc44d67ccdf
VibhashDwivedi <vibhashdwi1005@gmail.com>
2023-07-12 14:01:22 +0530

    map-filter

--- Section1/array_op.js
+++ Section1/array_op.js
@@ -17,3 +17,28 @@ for(let x of nums)
     console.log(emparray);
     emparray.push(x**2); 
 }
+console.log(emparray);
+console.log('----------------------------------');
+
+//map function
+const result = nums.map( (n)=>{ return n**2});
+console.log(result);
+
+
+const prices=[1329, 38267, 4002, 402, 5793, 352];
+console.log(prices.map( (n)=>{ return n*0.05+n}));
+
+const prices2 =['$2423.23', '$583.39', '$63.99','$23.99','$12.99','$900.9'];
+const res = prices2.map((n) => { return parseInt(n.slice(1))});
+console.log(res);
+console.log('-----------------------------------------');
+
+//using filter
+const filtered_prices = prices.filter((p)=> {return p>=500 &&p<=5000});
+console.log(filtered_prices);
+
+const laptop = ['Dell Vostro', 'Acer Predator', 'Asus Vivobook','HP Pavilion','Lenovo Thinkpad'];
+keyword='Dell';
+const searchData = laptop.filter((n)=>{ return  n.toLowerCase().includes(keyword.toLowerCase())}); 
+console.log(searchData);
+
