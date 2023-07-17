552a7567bb27784734e0e2fa25d31a3f2fd56174
VibhashDwivedi <vibhashdwi1005@gmail.com>
2023-07-17 19:39:55 +0530

    Practice

---
+++ Practice/newfolder/src/components/Navbar.jsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <NavLink className="navbar-brand" href="#">
+      Navbar
+    </NavLink>
+    <button
+      className="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span className="navbar-toggler-icon" />
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <NavLink className="nav-link "  to="/home">
+            Home
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/login">
+            Login
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/signup">
+            SignUp
+          </NavLink>
+        </li>
+        <li className=" dropdown">
+          <NavLink
+            className="nav-link dropdown-toggle"
+            href="#"
+            role="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            Dropdown
+          </NavLink>
+          <ul className="dropdown-menu">
+            <li>
+              <NavLink className="dropdown-item" href="#">
+                Action
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="dropdown-item" href="#">
+                Another action
+              </NavLink>
+            </li>
+            <li>
+              <hr className="dropdown-divider" />
+            </li>
+            <li>
+              <NavLink className="dropdown-item" href="#">
+                Something else here
+              </NavLink>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <NavLink className="nav-link disabled">Disabled</NavLink>
+        </li>
+      </ul>
+      <form className="d-flex" role="search">
+        <input
+          className="form-control me-2"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+        />
+        <button className="btn btn-outline-success" type="submit">
+          Search
+        </button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+  )
+}
+
+export default Navbar
