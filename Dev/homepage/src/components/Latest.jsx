a8c4f2b2fc8eae55cb67c63f5dad31694a30c765
VibhashDwivedi <vibhashdwi1005@gmail.com>
2023-08-28 07:46:46 +0530

    final commit

--- Dev/homepage/src/components/Latest.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Latest = () => {
-  return (
-    <div style={{marginTop:'100px'}}>Latest</div>
-  )
-}
-
-export default Latest
--- Dev/homepage/src/components/Signup.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Signup = () => {
-  return (
-    <div className='m-5'>Signup</div>
-  )
-}
-
-export default Signup
